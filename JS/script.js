d3de2ecb4de2469b6d6e6e24b0ef3e4ac6f10289
Mcl <nierimarcel@gmail.com>
2025-01-17 15:05:34 -0300

    new update code

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   // Função para atualizar a data e hora
   function updateDateTime() {
     const dateTimeElement = document.getElementById('data-hora');
     const now = new Date();
-    
+  
     // Formatando a data e hora como "dia/mês/ano - 00:00hrs"
     const day = String(now.getDate()).padStart(2, '0');
     const month = String(now.getMonth() + 1).padStart(2, '0'); // Mês começa do zero
@@ -25,14 +24,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (document.body.classList.contains('tema-claro')) {
       this.textContent = 'Tema Escuro';
-      document.getElementById('profile-image').src = '/assets/images/tema claro.png';
-      document.querySelector('.sobre img').src = '/assets/images/perfil-claro.png';
+      document.getElementById('profile-image').src = 'https://marcelnieridev.github.io/assets/images/tema_claro.png';
+      document.querySelector('.sobre img').src = 'https://marcelnieridev.github.io/assets/images/perfil-claro.png';
       document.querySelector('.conteudo-home .nome').classList.add('tema-claro');
       document.querySelector('.conteudo-home .descricao').classList.add('tema-claro');
     } else {
       this.textContent = 'Tema Claro';
-      document.getElementById('profile-image').src = '/assets/images/Acenando perfil.png';
-      document.querySelector('.sobre img').src = '/assets/images/perfil.png';
+      document.getElementById('profile-image').src = 'https://marcelnieridev.github.io/assets/images/Acenando_perfil.png';
+      document.querySelector('.sobre img').src = 'https://marcelnieridev.github.io/assets/images/perfil.png';
       document.querySelector('.conteudo-home .nome').classList.remove('tema-claro');
       document.querySelector('.conteudo-home .descricao').classList.remove('tema-claro');
     }
